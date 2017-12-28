fe9d3036caa7b7adc56da369b305bd59439277bf
MisterChing <chfnaruto@gmail.com>
2017-12-28 20:49:04 +0800

    fix bug

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 WORK_DIR=`dirname $0`
 HOME=$(readlink -f $(dirname $0))
-PHP="/home/users/chenghaofeng/athena/php/bin/php -c /home/users/chenghaofeng/athena/php/etc/php.ini"
+# PHP="/home/users/chenghaofeng/athena/php/bin/php -c /home/users/chenghaofeng/athena/php/etc/php.ini"
+PHP="/home/ching/athena/php/bin/php -c /home/ching/athena/php/etc/php.ini"
 export CLI_CRON_CONF="${HOME}/conf/CLI_SERVER_CONFIG"
 if [ $# -lt 1 ]; then
     echo 'too few param!';
     exit 1
 fi
 filename=$1".php"
-if [ ! -e script/${filename} ]; then
+if [ ! -e ${WORK_DIR}/script/${filename} ]; then
     echo "entry file dosen't exists."
     exit 1
 fi
+cd ${WORK_DIR}
 shift
 ${PHP} script/${filename} $@
 
